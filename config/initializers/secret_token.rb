213dc41f6896913aee5867a3010b400fa6aec05d
Mark Chavez <los_populares@yahoo.com>
2013-11-05 23:55:23 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = '53ee795892b8249acefc7d719811c1dee266b2682accfc8ad86e0359c95e86797a0da5326016cd543e522da9aca73c4cdd504b58847d77b4048cb9fd887879f8'
